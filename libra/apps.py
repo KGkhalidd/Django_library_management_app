221f9b68d06e790f30fa686e038c7e5556723d46
AymanRefat <aymanrefatpro@gmail.com>
2022-12-07 13:56:51 +0200

    Complete Changing Names , reset the migrations files

--- libra/apps.py
+++ libra/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class LmsAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'lms_app'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "libra"
